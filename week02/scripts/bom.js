0c2ae6aeb519bcd0adf2964a020100279be90c9e
KaylieT2025 <ktapia@byupathway.edu>
2026-03-11 18:51:56 -0400

    Working on adding list restrictions

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -4,26 +4,42 @@ const list = document.querySelector("#list");
 
 button.addEventListener("click", function () {
 
-    if (input.value.trim() !== "") {
-        input.value = input.value.trim();
-        const li = document.createElement("li");
-        const deletebutton = document.createElement("button");
-        li.textContent = input.value;
-        deletebutton.textContent = "❌";
-        deletebutton.setAttribute("aria-label", "Delete chapter");
-        deletebutton.addEventListener("click", function () {
-            list.removeChild(li);
+/*function addChapter() {
+    button.addEventListener("click", addChapter);
+    if (event.key === "Enter") {
+        addChapter();
+    }*/
+        if (list.children.length >= 10) {
+            alert("You can only add 10 chapters.");
             input.focus();
-        });
-        li.append(deletebutton);
-        list.append(li);
+            return;
+        }
 
-        input.value = "";
-        input.focus();
-    } else {
-        input.focus();
-        return;
-    };
+        if (input.value.trim() !== "") {
+            input.value = input.value.trim();
+            const li = document.createElement("li");
+            const deletebutton = document.createElement("button");
+            li.textContent = input.value;
+            deletebutton.textContent = "❌";
+            deletebutton.setAttribute("aria-label", "Delete chapter");
+            deletebutton.addEventListener("click", function () {
+                list.removeChild(li);
+                input.focus();
+            });
+            li.append(deletebutton);
+            list.append(li);
+
+            input.value = "";
+            input.focus();
+        } else {
+            input.focus();
+            return;
+        };
+    //};
+    /*button.addEventListener("click", addChapter);
+        if (event.key === "Enter") {
+            addChapter();
+            }*/
 
 });
 
