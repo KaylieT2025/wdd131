b3d172da2b49e5ad3d18dcd4219f6871d421ded1
KaylieT2025 <ktapia@byupathway.edu>
2026-03-11 18:00:48 -0400

    Updated js and html.

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -3,16 +3,27 @@ const button = document.querySelector("button");
 const list = document.querySelector("#list");
 
 button.addEventListener("click", function () {
-    
-    const li = document.createElement("li");
-    const deletebutton = document.createElement("button");
-    li.textContent = input.value;
-    deletebutton.textContent = "❌";
-    deletebutton.addEventListener("click", function () {
-        list.removeChild(li);
-    });
-    li.append(deletebutton);
-    list.append(li);
+
+    if (input.value.trim() !== "") {
+        input.value = input.value.trim();
+        const li = document.createElement("li");
+        const deletebutton = document.createElement("button");
+        li.textContent = input.value;
+        deletebutton.textContent = "❌";
+        deletebutton.setAttribute("aria-label", "Delete chapter");
+        deletebutton.addEventListener("click", function () {
+            list.removeChild(li);
+            input.focus();
+        });
+        li.append(deletebutton);
+        list.append(li);
+
+        input.value = "";
+        input.focus();
+    } else {
+        input.focus();
+        return;
+    };
 
 });
 
