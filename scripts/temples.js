808a92d50e858e799c526e54d911d118de0542b6
KaylieT2025 <ktapia@byupathway.edu>
2026-03-12 16:16:47 -0400

    Got HTML done and CSS mostly done.

---
+++ scripts/temples.js
@@ -0,0 +1,3 @@
+const d = new Date();
+document.getElementById("current-year").innerHTML = d.getFullYear();
+document.getElementById("lastModified").innerHTML = "Last Modified:" + document.lastModified;
