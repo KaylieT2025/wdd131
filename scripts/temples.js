ac787da692e160bf7c9007ffe9c75c5f5aa8970e
KaylieT2025 <ktapia@byupathway.edu>
2026-03-13 14:43:03 -0400

    Finished hamburger menu and put @media into temples-large.css

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,14 @@
+//For Last Modified date.
 const d = new Date();
 document.getElementById("current-year").innerHTML = d.getFullYear();
 document.getElementById("lastModified").innerHTML = "Last Modified:" + document.lastModified;
+
+//For hamburger menu.
+
+const mainnav = document.querySelector(".navigation")
+const hambutton = document.querySelector("#menu")
+
+hambutton.addEventListener("click", () => {
+    mainnav.classList.toggle("show");
+    hambutton.classList.toggle("show");
+});
