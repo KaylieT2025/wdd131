6bb47373126d74690f7feb2a44b07b6064aeee66
KaylieT2025 <ktapia@byupathway.edu>
2026-03-21 14:38:32 -0400

    Colors and html setup.

---
+++ scripts/place.js
@@ -0,0 +1,4 @@
+//For Last Modified date.
+const d = new Date();
+document.getElementById("current-year").innerHTML = d.getFullYear();
+document.getElementById("lastModified").innerHTML = "Last Modified:" + document.lastModified;
