a7af00a9b610d0bb046062626d9f2e2ce494fad4
KaylieT2025 <ktapia@byupathway.edu>
2026-04-08 08:10:15 -0400

    Meta desciption updated.

--- scripts/form.js
+++ scripts/form.js
@@ -26,20 +26,40 @@ const products = [
     }
 ];
 
+/* let count = localStorage.getItem("reviewCount") || 0;
+count++;
+localStorage.setItem("reviewCount", count);
+document.getElementById("reviewCounter").textContent = count; */
+
+const select = document.getElementById("productName");
+
+if (select) {
+    products.forEach(item => {
+
+        const opt = document.createElement("option");
+        opt.value = item.id;
+        opt.textContent = item.name;
+        select.appendChild(opt);
+    });
+}
+
 let count = localStorage.getItem("reviewCount") || 0;
 count++;
 localStorage.setItem("reviewCount", count);
 document.getElementById("reviewCounter").textContent = count;
 
-const select = document.getElementById("productName");
-products.forEach(item => {
-
-    const opt = document.createElement("option");
-    opt.value = item.id;
-    opt.textContent = item.name;
-    select.appendChild(opt);
-});
-
 const d = new Date();
 document.getElementById("current-year").innerHTML = d.getFullYear();
 document.getElementById("lastModified").innerHTML = "Last Modified:" + document.lastModified;
+
+const year = document.getElementById("current-year");
+if (year) {
+    
+    year.textContent = new Date().getFullYear();
+}
+
+const lastModified = document.getElementById("lastModified");
+if (modified) {
+    
+    modified.textContent = "Last Modified: " + document.lastModified;
+}
